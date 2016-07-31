2147006fac0da5bf406406dd5ceec4be39329a68
Thanmai Gajam <thanmai.gajam@gmail.com>
2016-07-31 13:27:40 +0530

    Lesson 3 - It Works! - DetailActivity works

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.sunshine.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
@@ -8,6 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class DetailActivity extends ActionBarActivity {
 
@@ -58,7 +60,14 @@ public class DetailActivity extends ActionBarActivity {
                                  Bundle savedInstanceState) {
 
             View rootView = inflater.inflate(R.layout.activity_detail, container, false);
-            return rootView;
+         Intent intent = getActivity().getIntent();
+            if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+                              String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+                             ((TextView) rootView.findViewById(R.id.textView))
+                                              .setText(forecastStr);
+
+
         }
+            return rootView;
     }
-}
+}}
