e71adbdc2a62abdbc9de6ce4daa8b4ae8f2b6c5d
Thanmai Gajam <thanmai.gajam@gmail.com>
2016-08-03 22:19:25 +0530

    Lesson 3 Temperature Units Setting

--- app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
@@ -25,7 +25,8 @@ public class SettingsActivity extends PreferenceActivity
 
         // For all preferences, attach an OnPreferenceChangeListener so the UI summary can be
         // updated when the preference changes.
-        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));    }
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_units_key)));   }
 
     /**
      * Attaches a listener so the summary is always updated with the preference value.
