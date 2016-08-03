d1ea4dd3301b26220da85fc8dc06dbb9a9cf1382
Thanmai Gajam <thanmai.gajam@gmail.com>
2016-08-03 21:49:18 +0530

    Lesson 3 - Temperature Units Setting

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -127,7 +127,16 @@ public class ForecastFragment extends Fragment {
         /**
          * Prepare the weather high/lows for presentation.
          */
-        private String formatHighLows(double high, double low) {
+
+
+            private String formatHighLows(double high, double low, String unitType) {
+
+                                    if (unitType.equals(getString(R.string.pref_units_imperial))) {
+                                   high = (high * 1.8) + 32;
+                                    low = (low * 1.8) + 32;
+                               } else if (!unitType.equals(getString(R.string.pref_units_metric))) {
+                                    Log.d(LOG_TAG, "Unit type not found: " + unitType);
+                               }
             // For presentation, assume the user doesn't care about tenths of a degree.
             long roundedHigh = Math.round(high);
             long roundedLow = Math.round(low);
@@ -175,6 +184,17 @@ public class ForecastFragment extends Fragment {
             dayTime = new Time();
 
             String[] resultStrs = new String[numDays];
+            // Data is fetched in Celsius by default.
+                        // If user prefers to see in Fahrenheit, convert the values here.
+                                // We do this rather than fetching in Fahrenheit so that the user can
+                                       // change this option without us having to re-fetch the data once
+                                                // we start storing the values in a database.
+                                                        SharedPreferences sharedPrefs =
+                                        PreferenceManager.getDefaultSharedPreferences(getActivity());
+                        String unitType = sharedPrefs.getString(
+                                       getString(R.string.pref_units_key),
+                                        getString(R.string.pref_units_metric));
+
             for (int i = 0; i < weatherArray.length(); i++) {
                 // For now, using the format "Day, description, hi/low"
                 String day;
@@ -202,7 +222,7 @@ public class ForecastFragment extends Fragment {
                 double high = temperatureObject.getDouble(OWM_MAX);
                 double low = temperatureObject.getDouble(OWM_MIN);
 
-                highAndLow = formatHighLows(high, low);
+                highAndLow = formatHighLows(high, low, unitType);
                 resultStrs[i] = day + " - " + description + " - " + highAndLow;
             }
 
@@ -225,7 +245,9 @@ public class ForecastFragment extends Fragment {
             String forecastJsonStr = null;
 
             String format = "json";
+
             String units = "metric";
+
             int numDays = 7;
             String appid = "22b8919e9ae48e9f6e0581a99bf49b15";
 
